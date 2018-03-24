8dd5958f0b5ed1ec35be32e532a8c7a159fec437
AbdAllah Abd-El-Fatah <AbdAllah.A.ELFatah@gmail.com>
2018-03-24 14:16:03 +0200

    Added Colors, closing #7. Updated DetailsHotelActivity.kt to handle image click, closing #9. Updated fonts, closing #1.
    Notes:-
    Tested on emulater, missing the image.

--- app/src/main/java/com/tajawa/abdallah/tajawal_android_task/Activitys/DetailsHotel/DetailsHotelActivity.kt
+++ app/src/main/java/com/tajawa/abdallah/tajawal_android_task/Activitys/DetailsHotel/DetailsHotelActivity.kt
@@ -1,8 +1,14 @@
 package com.tajawa.abdallah.tajawal_android_task.Activitys.DetailsHotel
 
+import android.content.res.Resources
 import android.graphics.Paint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.TypedValue
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import com.tajawa.abdallah.tajawal_android_task.Activitys.ImageLoaderUtil
 import com.tajawa.abdallah.tajawal_android_task.DataLayer.DataRepository
 import com.tajawa.abdallah.tajawal_android_task.DataLayer.Remote.Volley.RemoteDataSourceUsingVolley
@@ -11,6 +17,8 @@ import kotlinx.android.synthetic.main.activity_details_hotel.*
 
 
 class DetailsHotelActivity : AppCompatActivity(), DetailsHotelContract.View {
+
+    private var isImageFitScreen: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,5 +50,40 @@ class DetailsHotelActivity : AppCompatActivity(), DetailsHotelContract.View {
 
     override fun setHighRate(highRate: String) {
         tv_hotel_high_rate.text = highRate
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+        // Respond to the action bar's Up/Home button
+            android.R.id.home -> {
+                if (isImageFitScreen) {
+                    toggleImageFullScreen()
+                    return true
+                }
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // Note: 19-Jul-17 this might fail accourding to the following link "https://stackoverflow.com/questions/42673531/converting-dp-to-px-without-context"
+    private fun fromIntToDp(n: Int): Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, n.toFloat(), Resources.getSystem().displayMetrics).toInt()
+
+    private fun toggleImageFullScreen() {
+        if (isImageFitScreen) {
+            iv_hotel_image.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, fromIntToDp(250))
+            iv_hotel_image.adjustViewBounds = true
+            rv_hotel_data_without_image.visibility = View.VISIBLE
+        } else {
+            iv_hotel_image.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT)
+            iv_hotel_image.scaleType = ImageView.ScaleType.FIT_XY
+            iv_hotel_image.adjustViewBounds = true
+            rv_hotel_data_without_image.visibility = View.GONE
+        }
+        isImageFitScreen = !isImageFitScreen
+    }
+
+    fun onImageViewClick(view: View) {
+        toggleImageFullScreen()
     }
 }
